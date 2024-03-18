4a4c1beacef853c07be4e2e20bb3325be254747b
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-03-19 06:46:57 +0900

    Add:Docker files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db', // Docker Compose service name
+      port: 3306,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database_name',
+      entities: [User],
+      synchronize: true, // Note: Disable synchronize in production
+    }),
+  ],
 })
 export class AppModule {}
