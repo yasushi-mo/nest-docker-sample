8981a3e8673abcd8ae7a514dca13c499406ed0d7
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-03-20 06:34:38 +0900

    Update:DB settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,15 @@ import { UserController } from './user/user.controller';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'db', // Docker Compose service name
+      host: 'nest-docker-sample-db-1', // Docker Compose service name
       port: 3306,
-      username: 'your_username',
-      password: 'your_password',
-      database: 'your_database_name',
+      username: 'username',
+      password: 'password',
+      database: 'nest_app_db',
       entities: [User],
       synchronize: true, // Note: Disable synchronize in production
     }),
+    TypeOrmModule.forFeature([User]),
   ],
   controllers: [UserController],
   providers: [UserService],
